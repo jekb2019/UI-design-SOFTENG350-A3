d970583028ae079a7539fc50da1485f447b11ba1
Jason Ko <jekb2019@gmail.com>
2020-06-02 01:30:26 +1200

    Add JS share function

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Share Function
+const shareButton = document.querySelector(".share-btn");
+const sharePopup = document.querySelector("#share-popup");
+
+shareButton.addEventListener('click',openSharePopup);
+document.querySelector(".buttons__share").addEventListener('click',()=>{
+    closeSharePopup();
+    alert("Successfully Shared!");
+});
+document.querySelector(".buttons__cancel").addEventListener('click',closeSharePopup);
+document.querySelector(".x").addEventListener('click',closeSharePopup);
+
+function closeSharePopup(){
+    sharePopup.style.display = "none";
+};
+function openSharePopup(){
+    sharePopup.style.display = "flex";
+};
