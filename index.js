f61fe1cc6d8d242e1d645883c4f2a7579e4ff071
Jason Ko <jekb2019@gmail.com>
2020-06-02 01:58:02 +1200

    Add succeeded message

--- index.js
+++ index.js
@@ -1,15 +1,27 @@
 // Share Function
 const shareButton = document.querySelector(".share-btn");
 const sharePopup = document.querySelector("#share-popup");
+const successPopup = document.querySelector("#share-succeeded");
+const advancedShare = document.querySelector(".advanced span");
 
 shareButton.addEventListener('click',openSharePopup);
 document.querySelector(".buttons__share").addEventListener('click',()=>{
     closeSharePopup();
-    alert("Successfully Shared!");
+    openSuccessPopup();
 });
 document.querySelector(".buttons__cancel").addEventListener('click',closeSharePopup);
 document.querySelector(".x").addEventListener('click',closeSharePopup);
+document.querySelector(".ok").addEventListener('click',closeSuccessPopup);
+advancedShare.addEventListener('click',()=>{
+    alert("hi");
+});
 
+function closeSuccessPopup(){
+    successPopup.style.display = "none";
+}
+function openSuccessPopup(){
+    successPopup.style.display = "flex";
+}
 function closeSharePopup(){
     sharePopup.style.display = "none";
 };
