ae51853d8b090e7a866a5935a8b07c12947ef14f
Jason Ko <jekb2019@gmail.com>
2020-06-02 21:36:39 +1200

    Add advanced share function

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const shareButton = document.querySelector(".share-btn");
 const sharePopup = document.querySelector("#share-popup");
 const successPopup = document.querySelector("#share-succeeded");
 const advancedShare = document.querySelector(".advanced span");
+const advancedSharePopup = document.querySelector("#advanced-share");
+const advancedSharePopupOk = document.querySelector(".advanced-share__buttons__ok");
+const advancedSharePopupCancel = document.querySelector(".advanced-share__buttons__cancel");
 
 shareButton.addEventListener('click',openSharePopup);
 document.querySelector(".buttons__share").addEventListener('click',()=>{
@@ -12,10 +15,17 @@ document.querySelector(".buttons__share").addEventListener('click',()=>{
 document.querySelector(".buttons__cancel").addEventListener('click',closeSharePopup);
 document.querySelector(".x").addEventListener('click',closeSharePopup);
 document.querySelector(".ok").addEventListener('click',closeSuccessPopup);
-advancedShare.addEventListener('click',()=>{
-    alert("hi");
-});
+advancedShare.addEventListener('click',openAdvancedSharePopup);
+advancedSharePopupCancel.addEventListener('click', closeAdvancedSharePopup);
+advancedSharePopupOk.addEventListener('click',closeAdvancedSharePopup);
 
+function closeAdvancedSharePopup(){
+    advancedSharePopup.style.display = "none";
+    console.log("hi");
+}
+function openAdvancedSharePopup(){
+    advancedSharePopup.style.display = "block";
+}
 function closeSuccessPopup(){
     successPopup.style.display = "none";
 }
